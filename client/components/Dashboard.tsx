bed0c6176d354a50f4aa87f519a15c30ad63918f
Matthew Foley <matthewfoley333@gmail.com>
2025-11-04 12:54:15 +1300

    testing new remote

--- client/components/Dashboard.tsx
+++ client/components/Dashboard.tsx
@@ -1,26 +1,26 @@
-import { useTelemetry } from './Context.tsx'
+// import { useTelemetry } from './Context.tsx'
 
-function Dashboard() {
-  const { data, isPending, isError } = useTelemetry()
-  if (isPending) {
-    return <div>...Loading</div>
-  }
-  if (isError) {
-    return <div>Error</div>
-  }
-  return (
-    <div>
-      <h1>Telemetry Dashboard</h1>
-      {data.map((sensor, i: number) => (
-        <div key={i}>
-          <h3>{sensor.speed}</h3>
-          <p>Speed: {sensor.speed}</p>
-          <p>Temp: {sensor.temperature}</p>
-          <p>Battery: {sensor.battery}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
+// function Dashboard() {
+//   const { data, isPending, isError } = useTelemetry()
+//   if (isPending) {
+//     return <div>...Loading</div>
+//   }
+//   if (isError) {
+//     return <div>Error</div>
+//   }
+//   return (
+//     <div>
+//       <h1>Telemetry Dashboard</h1>
+//       {data.map((sensor, i: number) => (
+//         <div key={i}>
+//           <h3>{sensor.speed}</h3>
+//           <p>Speed: {sensor.speed}</p>
+//           <p>Temp: {sensor.temperature}</p>
+//           <p>Battery: {sensor.battery}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
 
-export default Dashboard
+// export default Dashboard
