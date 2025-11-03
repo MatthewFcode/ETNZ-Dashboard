073b0fd7dbd96b4607d666d06084b2a900126c17
Matthew Foley <matthewfoley333@gmail.com>
2025-11-04 00:18:23 +1300

    add .map() in the express route to loop over the returned array and convert values to camelCase

--- server/routes/telemetry.ts
+++ server/routes/telemetry.ts
@@ -1,8 +1,38 @@
 import { Router } from 'express'
-
 import * as db from '../db/telemetry.ts'
+import { TelemetryDataSnake } from '../../models/telemetry.ts'
 
 const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const snake = await db.getAllData()
+
+    const result = snake?.map(
+      ({
+        sensor_id,
+        time_stamp,
+        speed,
+        temperature,
+        battery,
+        altitude,
+      }: TelemetryDataSnake) => ({
+        sensorId: sensor_id,
+        timeStamp: time_stamp,
+        speed: speed,
+        temperature: temperature,
+        battery: battery,
+        altitude: altitude,
+      }),
+    )
+
+    console.log(result)
+    res.status(200).json(result)
+  } catch (err) {
+    res.status(200).json('Could not get all telemetry data')
+    console.log(err)
+  }
+})
 
 router.get('/:id', async (req, res) => {
   try {
