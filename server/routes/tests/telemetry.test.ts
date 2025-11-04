8580a261fe8d1a7a1a076f7988ef308391ed62f8
Matthew Foley <matthewfoley333@gmail.com>
2025-11-05 12:40:50 +1300

    feat: add the test for checking telemetry is updating hrough the back end routes

--- server/routes/tests/telemetry.test.ts
+++ server/routes/tests/telemetry.test.ts
@@ -1,13 +1,18 @@
 import { describe, it, expect, beforeEach, beforeAll, afterAll } from 'vitest'
 import request from 'supertest'
 import { StatusCodes } from 'http-status-codes'
-import { server } from '../../server.ts'
+import { server, wss } from '../../server.ts'
 import connection from '../../db/connection.ts'
+import { startTelemetryGeneration } from '../../telemetry/telemetry.js'
 
 const db = connection
 const BASE_URL = '/api/v1/telemetry'
 
+// helper function to help us wait for a certain period of time between making requests
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 beforeAll(async () => {
+  startTelemetryGeneration(wss)
   await db.migrate.latest()
 })
 
@@ -31,5 +36,33 @@ describe('tests that the database is returning random telemetry data', () => {
       battery: expect.any(Number),
       altitude: expect.any(Number),
     })
+  })
+})
+
+describe('tests the frequency of the telemetry stream', () => {
+  it('tests that the data being retrieved from the data base is changing over time', async () => {
+    const firstResponse = await request(server).get(`${BASE_URL}`)
+    expect(firstResponse.status).toBe(StatusCodes.OK)
+    expect(Array.isArray(firstResponse.body)).toBe(true)
+    const firstTelemetry = firstResponse.body[0]
+
+    await wait(600)
+
+    const secondResponse = await request(server).get(`${BASE_URL}`)
+    expect(secondResponse.status).toBe(StatusCodes.OK)
+    expect(Array.isArray(secondResponse.body)).toBe(true)
+    const secondTelemetry = secondResponse.body[0]
+
+    // assert the id of the first and second response to be the same
+    expect(firstTelemetry.sensorId).toBe(secondTelemetry.sensorId)
+
+    // compare the numerical values to ensure they are changing
+    const changed =
+      firstTelemetry.speed !== secondTelemetry.speed ||
+      firstTelemetry.temperature !== secondTelemetry.temperature ||
+      firstTelemetry.battery !== secondTelemetry.battery ||
+      firstTelemetry.altitude !== secondTelemetry.altitude
+
+    expect(changed).toBe(true)
   })
 })
