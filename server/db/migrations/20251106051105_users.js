51815b4aea7d96d430a7a22ac0a307dc08aa71db
Matthew Foley <matthewfoley333@gmail.com>
2025-11-06 18:24:03 +1300

    run users migration

---
+++ server/db/migrations/20251106051105_users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('role')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.dropTable('users')
+}
