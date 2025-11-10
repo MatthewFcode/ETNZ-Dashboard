e21f9783685f85e20af715698d997e4b17a1651e
Matthew Foley <matthewfoley333@gmail.com>
2025-11-10 23:11:48 +1300

    feat: add the auth0 connection on the backend

--- client/index.tsx
+++ client/index.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router/dom'
 import { createBrowserRouter } from 'react-router'
-
+import { Auth0Provider } from '@auth0/auth0-react'
 import routes from './routes.tsx'
 
 const queryClient = new QueryClient()
@@ -12,9 +12,19 @@ const router = createBrowserRouter(routes)
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
+    <Auth0Provider
+      domain="matthew-matai-2025.au.auth0.com"
+      clientId="zIkhlLSHHoLPSFnayPHy13B1dqj8DGKq"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: 'https://etnz/api',
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+      ,
+    </Auth0Provider>,
   )
 })
