a03ff0e90655981341dfa545269d2be9ab59774d
Matthew Foley <matthewfoley333@gmail.com>
2026-01-05 14:25:10 +1300

    add the generic mutation function the useQUery for gettting users by their ID and then also add the wrapper POST function for adding users

---
+++ client/hooks/useUsers.ts
@@ -0,0 +1,46 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  MutationFunction,
+} from '@tanstack/react-query'
+import { useAuth0 } from '@auth0/auth0-react'
+import { User } from '../../models/users.ts'
+import { getUserById, postUser, updateUser, deleteUser } from '../apis/users.ts'
+
+export function useGetUserByAuth0Id() {
+  const { user, getAccessTokenSilently } = useAuth0()
+  const query = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently()
+      return getUserById(token)
+    },
+    enabled: !!user,
+  })
+  return query
+}
+
+export function useUserMutation<TData = unknown, TVariables = unknown>(
+  mutationFn: MutationFunction<TData, TVariables>,
+) {
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] })
+    },
+  })
+
+  return mutation
+}
+
+//POST mutation wrapper function
+export function usePostUser() {
+  const { getAccessTokenSilently } = useAuth0()
+
+  return useUserMutation(async (newUser: User) => {
+    const token = await getAccessTokenSilently()
+    return postUser(newUser, token)
+  })
+}
