a6942891a8e4a066d3663cc23d2d414077e2ae60
Matthew Foley <matthewfoley333@gmail.com>
2026-01-05 19:23:07 +1300

    finish the rest of the registration form

--- client/components/Registration.tsx
+++ client/components/Registration.tsx
@@ -1,12 +1,12 @@
 import { usePostUser } from '../hooks/useUsers.ts'
 import { useNavigate } from 'react-router'
-import { useAuth0 } from '@auth0/auth0-react'
-import {useState} from 'react'
+//import { useAuth0 } from '@auth0/auth0-react'
+import { useState } from 'react'
 import { IfAuthenticated, IfNotAuthenticated } from './Auth0.tsx'
 
 function Registraton() {
   const postUser = usePostUser()
-  const {getAccessTokenSilently} = useAuth0()
+  //const { getAccessTokenSilently } = useAuth0()
   const navigate = useNavigate()
 
   const [form, setForm] = useState({
@@ -29,15 +29,14 @@ function Registraton() {
   //   }
   // }
   const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
-    const token = await getAccessTokenSilently()
+    // const token = await getAccessTokenSilently()
     evt.preventDefault()
 
-   const newUser = {
-    name: form.name,
-    role: form.role,
-    //profile_photo: form.file
-   }
-
+    const newUser = {
+      name: form.name,
+      role: form.role,
+      //profile_photo: form.file,
+    }
 
     postUser.mutate(newUser, {
       onSuccess: () => {
@@ -52,7 +51,54 @@ function Registraton() {
   }
 
   return (
-    
+    <>
+      <IfAuthenticated>
+        <h1>🏎️ Create your ETNZ race hub account 🏎️ </h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">What is your name?</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="..Matthew"
+            />
+          </div>
+          <div>
+            <label htmlFor="role">What is your role?</label>
+            <input
+              type="text"
+              id="role"
+              name="role"
+              value={form.role}
+              onChange={handleChange}
+              placeholder="..Captain"
+            />
+          </div>
+
+          {/* <div>
+          <label htmlFor="file">Upload a file..</label>
+          <input
+            type="file"
+            id="file"
+            name="file"
+            value={form.file}
+            onChange={handleFileChange}
+          />
+        </div> */}
+          <button type="submit" disabled={!form.name}>
+            ⛵Create Account⛵
+          </button>
+        </form>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <div>
+          <h1>❗Go back and authenticate yourself❗</h1>
+        </div>
+      </IfNotAuthenticated>
+    </>
   )
 }
 
