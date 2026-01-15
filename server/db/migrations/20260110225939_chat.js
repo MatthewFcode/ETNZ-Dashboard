3bbfc482be58123fdaef862eeba507f70a077325
Matthew Foley <matthewfoley333@gmail.com>
2026-01-15 15:04:16 +1300

    add the knex ORM functions for the chat

--- server/db/migrations/20260110225939_chat.js
+++ server/db/migrations/20260110225939_chat.js
@@ -5,7 +5,7 @@
 export async function up(knex) {
   await knex.schema.createTable('chat', (table) => {
     table.increments('id').primary()
-    table.string('messages')
+    table.string('message')
     table.timestamp('time_sent').defaultTo(knex.fn.now())
     table.string('auth0Id').references('users.auth0Id').onDelete('CASCADE') // references the users table .where() for where it is the same and gather user info for each message
   })
