5eac327c3350b1d2cf99aad04d6b4d9ac90a8ec5
Matthew Foley <matthewfoley333@gmail.com>
2026-02-11 00:18:23 +1300

    add the formdata mismatch name between the express and react component

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
 import { server, wss } from './server.ts'
 import { startTelemetryGeneration } from './telemetry/telemetry.js'
-import { chatGenerator } from './ai-chats/ai-chat.ts'
+//import { chatGenerator } from './ai-chats/ai-chat.ts'
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Server listening on port', PORT)
   startTelemetryGeneration(wss)
-  chatGenerator()
+  //chatGenerator()
 })
